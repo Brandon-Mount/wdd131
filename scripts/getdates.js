57d1e89c9f38c6451ee4eca364927310b626f8fc
Brandon-Mount <bmount44@gmail.com>
2025-01-12 15:43:10 -0700

    adjusted some code

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,8 +1,9 @@
-// Dynamically populate the current year in the footer's first paragraph
+// Dynamically populate the current year in the footer
 const currentYear = new Date().getFullYear();
 document.getElementById('currentyear').textContent = currentYear;
 
-// Dynamically populate the last modified date in the footer's second paragraph
-document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
+// Dynamically populate the last modified date in the footer
+const lastModifiedDate = new Date(document.lastModified);
+document.getElementById('lastModified').textContent = 'Last Modification: ' + lastModifiedDate.toLocaleString();
 
 
